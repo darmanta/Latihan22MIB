03c2d83b6055850e18ca4ea22e3bb67ca9aec68e
Darman <71362875+darmanta@users.noreply.github.com>
2024-02-28 20:38:05 +0700

    Update index.php
    Penambahan kode nama kelas.

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 echo "Pengenalan Github<br>";
 echo "NIM : 1014078301<br>";
-echo "Nama Lengkap : Darmanta Sukrianto";
+echo "Nama Lengkap : Darmanta Sukrianto<br>";
+echo "Kelas : 22 MI B";
 ?>
